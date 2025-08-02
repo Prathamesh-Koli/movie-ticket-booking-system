c389b96d7daf6602ea54cd80db6180e7a5cdd6b8
Pratik Shingate <pratikshingate777@gmail.com>
2025-08-02 17:46:51 +0530

    Update Application.java for additional configuration

--- Code/Backend/spring_boot_backend/src/main/java/com/bookar/Application.java
+++ Code/Backend/spring_boot_backend/src/main/java/com/bookar/Application.java
@@ -1,6 +1,7 @@
 package com.bookar;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean 
 	public ModelMapper modelMapper() {
 		System.out.println("in model mapper creation");
 		ModelMapper mapper = new ModelMapper();
@@ -21,5 +24,7 @@ public class Application {
 		return mapper;
 
 	}
+	
+	
 
 }
