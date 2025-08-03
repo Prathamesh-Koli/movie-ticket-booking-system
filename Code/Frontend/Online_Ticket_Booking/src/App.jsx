56a6bbac59636f27a2bcb11d08163460ecf3a9fd
Koli Prathamesh Prakash <prathameshkoli001@gmail.com>
2025-08-03 13:18:30 +0530

    Add route for seatSelection Page

--- Code/Frontend/Online_Ticket_Booking/src/App.jsx
+++ Code/Frontend/Online_Ticket_Booking/src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import ShowSelectionPage from "./pages/ShowSelectionPage"
+import { BookingProvider } from "./contexts/BookingContext"
+import SeatSelectionPage from "./pages/SeatSelectionPage"
 
 function App() {
   return (
-    <Router>
+    <BookingProvider>
+      <Router>
       <div className="app">
         {/* <Navbar /> */}
         <main className="main-content">
           <Routes>
-            <Route path="/movie/:id/shows" element={<ShowSelectionPage />} />
+            <Route path="/movie/:id/shows" element={<ShowSelectionPage />}
+             />
+            <Route path="/movie/:id/seats" element={<SeatSelectionPage />} />
           </Routes>
         </main>
         {/* <Footer /> */}
       </div>
     </Router>
+    </BookingProvider>
+    
   )
 }
 
