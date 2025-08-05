c14e044c663a82f9d20baa2b0b693025720df9c5
Vijay Patil <vijaypatil@9128gmail.com>
2025-08-05 23:03:12 +0530

    update application.java

--- Code/Backend/spring_boot_backend/src/main/java/com/bookar/Application.java
+++ Code/Backend/spring_boot_backend/src/main/java/com/bookar/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.bookar.dto.MovieResponseDTO;
+import com.bookar.entities.Movie;
+
 @SpringBootApplication
 public class Application {
 
@@ -20,6 +23,11 @@ public class Application {
 		mapper.getConfiguration()
 				.setMatchingStrategy(MatchingStrategies.STRICT)
 				.setPropertyCondition(Conditions.isNotNull());
+		
+		mapper.typeMap(Movie.class, MovieResponseDTO.class)
+        .addMapping(Movie::getMovieId, MovieResponseDTO::setMovieId)
+        .addMapping(Movie::getPosterUrl, MovieResponseDTO::setPosterUrl)
+        .addMapping(Movie::getGenres, MovieResponseDTO::setGenres);
 		return mapper;
 
 	}
