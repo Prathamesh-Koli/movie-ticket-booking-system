92b49c67930365098b8811bc5cde17aa67df16bc
Vijay Patil <vijaypatil@9128gmail.com>
2025-08-05 23:00:26 +0530

    add service required for Home page and Search page functionality

--- Code/Backend/spring_boot_backend/src/main/java/com/bookar/service/MovieService.java
+++ Code/Backend/spring_boot_backend/src/main/java/com/bookar/service/MovieService.java
@@ -2,14 +2,19 @@ package com.bookar.service;
 
 import java.util.List;
 import com.bookar.dto.MovieDetailDTO;
-
+import com.bookar.dto.MovieHomeResponseDTO;
 import com.bookar.dto.MovieResponseDTO;
+import com.bookar.entities.Genre;
+import com.bookar.entities.MovieStatus;
 
 public interface MovieService {
-	MovieResponseDTO getMovieById(Long id);
-	
+	MovieResponseDTO getMovieById(Long id);	
 	List<String> getLocationsForMovie(Long movieid);
 	MovieDetailDTO getMovieDetailsById(Long id);
+	List<MovieHomeResponseDTO> getAllMovies();
+    List<MovieHomeResponseDTO> getMoviesByGenre(List<Genre> genres);
+    List<MovieHomeResponseDTO> getMoviesByStatus(MovieStatus status);
 
 
 }
+
--- Code/Backend/spring_boot_backend/src/main/java/com/bookar/service/MovieServiceImpl.java
+++ Code/Backend/spring_boot_backend/src/main/java/com/bookar/service/MovieServiceImpl.java
@@ -1,4 +1,5 @@
 package com.bookar.service;
+
 import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -7,41 +8,67 @@ import com.bookar.custom_exceptions.ResourceNotFoundException;
 import com.bookar.dao.MovieDao;
 import com.bookar.dao.ShowDao;
 import com.bookar.dto.MovieDetailDTO;
+import com.bookar.dto.MovieHomeResponseDTO;
 import com.bookar.dto.MovieResponseDTO;
 import com.bookar.dto.MovieCastDTO;
+import com.bookar.entities.Genre;
 import com.bookar.entities.Movie;
 import com.bookar.entities.MovieCast;
+import com.bookar.entities.MovieStatus;
 
 import lombok.AllArgsConstructor;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class MovieServiceImpl implements MovieService {
-    private final MovieDao movieDao;
-    private final ShowDao showDao;
-    private final ModelMapper modelMapper;
+	private final MovieDao movieDao;
+	private final ShowDao showDao;
+	private final ModelMapper modelMapper;
 
-    @Override
-    public MovieResponseDTO getMovieById(Long id) {
-        Movie movie = movieDao.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Movie not found with id: " + id));
-        return modelMapper.map(movie, MovieResponseDTO.class);
-    }
+	@Override
+	public MovieResponseDTO getMovieById(Long id) {
+		Movie movie = movieDao.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Movie not found with id: " + id));
+		return modelMapper.map(movie, MovieResponseDTO.class);
+	}
 
-    @Override
-    public List<String> getLocationsForMovie(Long movieId) {
-        return showDao.findDistinctLocationsByMovieId(movieId);
-    }
+	@Override
+	public List<String> getLocationsForMovie(Long movieId) {
+		return showDao.findDistinctLocationsByMovieId(movieId);
+	}
 
-    @Override
-    public MovieDetailDTO getMovieDetailsById(Long id) {
-        Movie movie = movieDao.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Movie not found with id: " + id));
+	@Override
+	public MovieDetailDTO getMovieDetailsById(Long id) {
+		Movie movie = movieDao.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Movie not found with id: " + id));
 
-       
-        modelMapper.typeMap(MovieCast.class, MovieCastDTO.class);
+		modelMapper.typeMap(MovieCast.class, MovieCastDTO.class);
 
-        return modelMapper.map(movie, MovieDetailDTO.class);
-    }
+		return modelMapper.map(movie, MovieDetailDTO.class);
+	}
+
+	@Override
+	@Transactional
+	public List<MovieHomeResponseDTO> getAllMovies() {
+		return movieDao.findAll().stream().map(movie -> modelMapper.map(movie, MovieHomeResponseDTO.class))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<MovieHomeResponseDTO> getMoviesByGenre(List<Genre> genres) {
+		return movieDao.findByGenresIn(genres).stream().map(movie -> modelMapper.map(movie, MovieHomeResponseDTO.class))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<MovieHomeResponseDTO> getMoviesByStatus(MovieStatus status) {
+		return movieDao.findByStatus(status).stream().map(movie -> modelMapper.map(movie, MovieHomeResponseDTO.class))
+				.collect(Collectors.toList());
+	}
 }
