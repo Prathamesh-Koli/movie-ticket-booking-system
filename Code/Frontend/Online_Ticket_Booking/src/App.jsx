91342c538833ea256d766ed051c9ba05def09e65
Vijay Patil <vijaypatil@9128gmail.com>
2025-08-05 23:23:21 +0530

    restructure app.jsx

--- Code/Frontend/Online_Ticket_Booking/src/App.jsx
+++ Code/Frontend/Online_Ticket_Booking/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import { ToastContainer } from 'react-toastify';
 import ShowSelectionPage from "./pages/ShowSelectionPage"
 import { BookingProvider } from "./contexts/BookingContext"
@@ -6,27 +6,40 @@ import SeatSelectionPage from "./pages/SeatSelectionPage"
 import Login from './pages/Login';
 import Register from './pages/Register';
 import UserProfilePage from "./pages/UserProfilePage";
+import { AuthProvider } from "./contexts/AuthContext"
+import Navbar from "./components/layout/Navbar"
+import { ThemeProvider } from "./contexts/ThemeContext"
+import HomePage from "./pages/HomePage"
+import SearchPage from "./pages/SearchPage"
 
 function App() {
   return (
-    <BookingProvider>
-      <Router>
-      <div className="app">
-        {/* <Navbar /> */}
-        <main className="main-content">
-          <Routes>
-            <Route path="/movie/:id/seats" element={<SeatSelectionPage />} />
-            <Route path="/movie/:id/shows" element={<ShowSelectionPage />} />
-            <Route path='signin' element={<Login/>}/>
-            <Route path='profile' element={<UserProfilePage/>}/>
-            <Route path="signup" element={<Register/>}/>
-          </Routes>
-        </main>
-        {/* <Footer /> */}
-        <ToastContainer/>
-      </div>
-    </Router>
-    </BookingProvider>
+    <ThemeProvider>
+      <AuthProvider>
+      <BookingProvider>
+        <Router>
+          <div className="app">
+            <Navbar />
+            <main className="main-content">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+
+                <Route path="/movie/:id/seats" element={<SeatSelectionPage />} />
+                <Route path="/movie/:id/shows" element={<ShowSelectionPage />} />
+                <Route path='signin' element={<Login />} />
+                <Route path='profile' element={<UserProfilePage />} />
+                <Route path="signup" element={<Register />} />
+                <Route path="/search" element={<SearchPage />} />
+                {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+              </Routes>
+            </main>
+            {/* <Footer /> */}
+            <ToastContainer />
+          </div>
+        </Router>
+      </BookingProvider>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
