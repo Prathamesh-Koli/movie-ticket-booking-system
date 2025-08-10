80fca09d005c6f3cc691b74df5791fd63f955290
Pratik Shingate <pratikshingate777@gmail.com>
2025-08-10 15:17:34 +0530

    Created TheaterStatus for Theater owner and Admin functionality

---
+++ Code/Backend/spring_boot_backend/src/main/java/com/bookar/service/TheaterService.java
@@ -0,0 +1,18 @@
+package com.bookar.service;
+
+import java.util.List;
+import com.bookar.dto.TheaterRequestDTO;
+import com.bookar.dto.TheaterResponseDTO;
+import com.bookar.entities.Theater;
+import com.bookar.entities.TheaterStatus;
+
+public interface TheaterService {
+    Theater addTheater(TheaterRequestDTO dto);
+    List<Theater> getTheatersByStatus(TheaterStatus status);
+    List<Theater> getTheatersByOwnerAndStatus(Long ownerId, TheaterStatus status);
+    List<Theater> getAllTheaters();
+    Theater updateTheaterStatus(Long theaterId, TheaterStatus status);
+    void deleteTheater(Long theaterId);
+    TheaterResponseDTO getTheaterDetails(Long theaterId);
+
+}
